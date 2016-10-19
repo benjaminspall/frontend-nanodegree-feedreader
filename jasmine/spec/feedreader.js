a9941ff92ab7da289380807ddcb3f19e4e42e9d9
Benjamin <benjamin.spall@gmail.com>
2016-10-19 16:07:08 -0700

    Feed and menu tests completed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,17 +9,14 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
+    /* This is the first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
+        /* This is the first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * empty.
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -27,32 +24,54 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* This is the second test - it tests to make sure that each
+         * feed in the allFeeds object has a URL defined and that it
+         * is not empty.
          */
+         it("should have a defined URL", function() {
+           // Loops through all the feed objects in allFeeds
+           for (i = 0; i < allFeeds.length; i++) {
+             expect(allFeeds[i].url).not.toBe(undefined);
+             expect(allFeeds[i].url).not.toBe('');
+           }
+         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* This is the third test - it tests to make sure that each
+         * feed in the allFeeds object has a name defined and that
+         * it is not empty.
          */
+         it("should have a defined name", function() {
+           // Loops through all the feed objects in allFeeds
+           for (i = 0; i < allFeeds.length; i++) {
+             expect(allFeeds[i].name).not.toBe(undefined);
+             expect(allFeeds[i].name).not.toBe('');
+           }
+         });
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
+    /* This is the second test suite. This suite is all about the
+     * menu element in our application.
+     */
+    describe('The menu', function() {
+         /* This is the fourth test - it tests to ensure that the
+          * menu element is hidden by default.
           */
+         it('should be hidden by default', function() {
+            expect($(document.body).hasClass('menu-hidden')).toBe(true);
+        });
+
+         /* This is the fifth test - it tests to ensure that the
+          * menu changes visibility when the menu icon is clicked.
+          */
+          it('should change visibility when clicked', function() {
+            menuIcon = $('.menu-icon-link');
+            expect($(document.body).hasClass('menu-hidden')).toBe(true);
+            menuIcon.click();
+            expect($(document.body).hasClass('menu-hidden')).toBe(false);
+            menuIcon.click();
+            expect($(document.body).hasClass('menu-hidden')).toBe(true);
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
